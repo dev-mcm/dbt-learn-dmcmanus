b1024710111141ee85604b60cd8b86d739b58d35
Devin McManus <dmcmanus@bluestate.co>
2020-04-30 17:16:30 +0000

    added dbt utils

--- models/staging/stripe/stg_stripe_payments.sql
+++ models/staging/stripe/stg_stripe_payments.sql
@@ -2,6 +2,7 @@ SELECT
     p.ID as payment_id
     ,p."orderID" as order_id
     ,p."paymentMethod" as payment_method
+    ,{{ cents_to_decimal('amount') }} as macro_amount
     ,p.amount/100 as amount
     ,p.created as created_dt
     ,p._batched_at as _batched_at_dt
